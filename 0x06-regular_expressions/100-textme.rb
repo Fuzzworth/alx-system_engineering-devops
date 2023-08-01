49f33c1895ebc5d93939b78f1dbf602d16e48a81
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-08-01 21:40:30 +0200

    Task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 if ARGV.length == 1
-  sender, number, flags = ARGV[0].match(/^.*\[from:\s([a-z|A-Z]*)\]\s\[to:\s(\+\d*)\]\s\[flags:\s(.*)?\]$/)
+  sender, number, flags = ARGV[0].match(/^.*\[from:\s([a-z|A-Z]*)\]\s\[to:\s(\+\d*)\]\s\[flags:\s(.*)?\]$/).captures
   puts "#$sender,#$number,#$flags"
   exit
 end
