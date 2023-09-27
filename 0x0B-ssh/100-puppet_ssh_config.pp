d53bf93ad1f7a11fe7210ca1fba93638523fc7bf
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-09-27 03:53:18 +0200

    task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# Docs 
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
