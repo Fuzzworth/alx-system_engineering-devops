db2034fbf84846e5a86d43314608261058e4438a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-03 02:48:16 +0200

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,17 @@
-exec { '/usr/bin/env apt-get -y update' : }
--> package { 'nginx' :
-  ensure => installed,
+package { 'nginx':
+  ensure => 'installed',
 }
--> file { '/var/www/html/index.html' :
-  content => 'Holberton School!',
+
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
 }
--> file_line { 'add header':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "\tadd_header X-Served-By ${hostname};",
-  after  => 'server_name _;',
-}
--> service { 'nginx':
-  ensure => running,
+
+file_line { 'Creating header':
+  	ensure => present,
+  	path   => '/etc/nginx/sites-available/default',
+  	line   => "\tadd_header X-Served-By ${hostname};",
+  	after  => 'server_name _;',
+	require => Service['nginx'],
 }
