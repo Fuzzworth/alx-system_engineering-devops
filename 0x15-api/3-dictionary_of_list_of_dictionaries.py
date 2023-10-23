f8765feb85b9a8a34159d8b54fbb353f511c8ced
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-24 00:05:59 +0200

    task 4

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    
+    users_j = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    todos_j = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    todos = {}
+
+    for user in users_j:
+        tasks = []
+        for task in todos_j:
+            if task.get('userId') == user.get('id'):
+                task_dict = {
+                        "username": user.get('username'),
+                        "task": task.get('title'),
+                        "completed": task.get('completed')
+                        }
+                tasks.append(task_dict)
+        todos[user.get('id')] = tasks
+
+    with open('todo_all_employees.json', mode='w') as file:
+        json.dump(todos, file)
