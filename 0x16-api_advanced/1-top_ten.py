0d6bcbd8a182476c1fdff8a1340bb54d5e99dfd9
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-11-08 00:05:15 +0200

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Function Docs
+    """
+    url = 'https://www.reddit.com'
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    sort = 'top'
+    limit = 10
+    response = requests.get('{}/r/{}/.json?sort={}&limit={}'.format(
+        url,subreddit,'top',10),
+        headers=headers,
+        allow_redirects=False)
+    if response.status_code == 200:
+        for post in response.json()['data']['children'][0:10]:
+            print(post['data']['title'])
+    else:
+        print(None)
