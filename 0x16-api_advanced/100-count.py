1a07a974fc739fa7f0fa9c5a49079d8462646df9
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-11-08 01:09:15 +0200

    Task 3

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -14,6 +14,22 @@ def count_words(subreddit, word_list, instances={}, count=0, after=""):
         'Accept': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     }
+    
+    if not instances:
+        for word in word_list:
+            if word.lower() not in instances:
+                instances[word.lower()] = 0
+
+    if after is None:
+        instances = sorted(
+                instances.items(),
+                key=lambda key_value: (-key_value[1], key_value[0])
+                )
+        for instance in instances:
+            if instance[1]:
+                print("{}: {}".format(instance[0], instance[1]))
+        return None
+
     response = requests.get(
             '{}/r/{}/.json?sort={}&limit={}&count={}&after={}'.format(
                 url, subreddit, 'hot', 100, count, after),
@@ -26,23 +42,6 @@ def count_words(subreddit, word_list, instances={}, count=0, after=""):
         print("")
         return None
 
-    if not instances:
-        for word in word_list:
-            if word.lower() not in instances:
-                instances[word.lower()] = 0
-
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(
-                instances.items(),
-                key=lambda key_value: (-key_value[1], key_value[0])
-                )
-        for instance in instances:
-            if instance[1]:
-                print("{}: {}".format(instance[0], instance[1]))
-        return None
     try:
         hot = response.json()['data']['children']
         aft = response.json()['data']['after']
