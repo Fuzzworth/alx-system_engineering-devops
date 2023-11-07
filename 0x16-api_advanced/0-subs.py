93f7f300551fceaa871c4b3ba7681649630f7879
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-11-07 23:59:13 +0200

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Function Docs
+    """
+    url = 'https://www.reddit.com'
+    header = {
+        'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    }
+    response = requests.get('{}/r/{}/about/.json'.format(url, subreddit),
+        headers=header,
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    return 0
