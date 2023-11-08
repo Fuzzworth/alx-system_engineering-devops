df24fa262f50f2c8a3795322026db0f35f1464bb
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-11-09 00:41:28 +0200

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -8,6 +8,6 @@ file { '/var/www/html/wp-settings.php':
 service { 'apache2':
   ensure    => 'running',
   enable    => true,
-  provider  => 'systemd', # Use the appropriate provider for your system.
+  provider  => 'service', # Use the appropriate provider for your system.
   subscribe => File['/var/www/html/wp-settings.php'], # Ensure the service restarts when the file changes.
 }
