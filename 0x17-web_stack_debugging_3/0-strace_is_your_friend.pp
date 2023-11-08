ffb57f20a26b7d4bb1f5c5b0aa5f44ad3e12ad3a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-11-09 00:51:17 +0200

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,13 @@
-# Step 1: Modify the wp-settings.php file
-exec { 'modify-wp-settings':
-  command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  onlyif  => "test -e /var/www/html/wp-settings.php",
-  require => File['/var/www/html/wp-settings.php'], # Ensure this runs after the file is present.
+# Ensure the replacement is made in the wp-settings.php file
+file { '/var/www/html/wp-settings.php':
+  ensure  => present,
+  content => replace('class-wp-locale.phpp', 'class-wp-locale.php'),
 }
 
-# Step 2: Restart the Apache2 service using Upstart
-exec { 'restart-apache':
-  command     => 'initctl restart apache2',
-  path        => '/sbin:/usr/sbin:/bin',
-  refreshonly => true, # Only run this command when notified by another resource.
-  subscribe   => Exec['modify-wp-settings'], # Subscribe to the previous step.
+# Restart the Apache2 service
+service { 'apache2':
+  ensure    => 'running',
+  enable    => true,
+  provider  => 'service', # Use the appropriate provider for your system.
+  subscribe => File['/var/www/html/wp-settings.php'], # Ensure the service restarts when the file changes.
 }
